6a268786241a46b41ca521c65a566779f651504f
Dam17an <tomcdamijan@gmail.com>
2025-05-03 17:17:34 +0200

    done Crew/RWD

--- space-neo/src/App.js
+++ space-neo/src/App.js
@@ -1,10 +1,13 @@
 import Hero from './Components/Hero/Hero.jsx';
+import Crew from './Components/Crew/Crew.jsx';
 
 function App() {
-  return (
-      <div className="App">
-        <Hero />
-      </div>
-  );
+    return (
+        <div className="App">
+            <Hero />
+            <Crew />
+        </div>
+    );
 }
+
 export default App;
---
+++ space-neo/src/Components/Crew/Crew.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Crew.css';
+import bg from './rocket-bg2.png';
+
+<img src="rocket-bg2.png" alt="Crew" className="crew-image"/>
+
+const Crew = () => {
+    return (
+        <div className="crew-content">
+            <p className="subtitle">NEW HAWK Q8 </p>
+            <h1 className="title">CREW MISSION 5</h1>
+            <p className="underwrite-line">LEARN ABOUT THE MISSION</p>
+        </div>
+    );
+};
+
+export default Crew;
