7022129db759ae25d957fba6cd8b7a4526add6b4
Dam17an <tomcdamijan@gmail.com>
2025-05-04 10:43:26 +0200

    Done Launch/RWD

--- space-neo/src/App.js
+++ space-neo/src/App.js
@@ -1,11 +1,13 @@
 import Hero from './Components/Hero/Hero.jsx';
 import Crew from './Components/Crew/Crew.jsx';
+import Launch from './Components/Launch/Launch.jsx';
 
 function App() {
     return (
         <div className="App">
             <Hero />
             <Crew />
+            <Launch />
         </div>
     );
 }
---
+++ space-neo/src/Components/Launch/Launch.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Launch.css';
+import bg from './rocket-bg3.png';
+
+const Launch = () => {
+    return (
+        <div className="launch-section">
+            {}
+            <div className="launch-text">LAUNCH</div>
+            <div className="launch-inner">
+                <div className="launch-content">
+                    <h2 className="launch-heading">GET INVOLVED</h2>
+                    <p className="launch-paragraph">
+                        Lorem ipsum text as placeholder for this section of text. To be filled in later with something meaningful and space themed. Lorem ipsum text as placeholder for this section of text. To be filled in later with something meaningful and space themed. Lorem ipsum text as placeholder for this section of text. To be filled in later with something meaningful and space themed.
+
+                        Lorem ipsum text as placeholder for this section of text. To be filled in later.
+                    </p>
+                    <button className="launch-button">GET INVOLVED</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Launch;
